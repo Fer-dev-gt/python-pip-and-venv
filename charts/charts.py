67a9abdb61a483a88d63c9d6d231f88f66bee627
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-12-26 21:23:20 -0600

    updated .gitignore and added commentary

--- charts/charts.py
+++ charts/charts.py
@@ -4,7 +4,7 @@ def generate_pie_chart():
     labels = ['Python', 'C++', 'Ruby', 'Java']
     values = [90, 80, 75, 95]
 
-    fig, ax = plt.subplots()
-    ax.pie(values, labels=labels)
+    fig, ax = plt.subplots()            # fig es la figura, ax es el plano
+    ax.pie(values, labels=labels)       # ax.pie genera el gráfico de torta, le pasamos los valores y las etiquetas
     plt.savefig('pie.png')
     plt.close()
